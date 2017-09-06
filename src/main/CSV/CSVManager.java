35dd21c22e6292b22bdab29c02112a8994118e2a
bms112 <bms112@imperial.ac.uk>
2017-09-06 15:08:31 +0100

    Implemented Reading the CSV File line by line.

--- src/main/CSV/CSVManager.java
+++ src/main/CSV/CSVManager.java
@@ -1,11 +1,16 @@
 package CSV;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CSVManager {
 
     private static CSVManager csvManager;
+    private String path;
 
     private CSVManager() {
-
+        path = "C:\\Users\\Ben\\IdeaProjects\\My_Mobile_Workers_Technical_Test\\src\\resources\\jobdata.csv";
     }
 
     public static CSVManager getInstance() {
@@ -13,5 +18,24 @@ public class CSVManager {
             csvManager = new CSVManager();
         }
         return csvManager;
+    }
+
+    public void readCSVFile() {
+        String line;
+        String splitChar = ",";
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            line = br.readLine();
+            if(checkColumnNames(line)) {
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private boolean checkColumnNames(String line) {
+        return true;
     }
 }
